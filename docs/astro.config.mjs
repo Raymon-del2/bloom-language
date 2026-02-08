bd65541980ba089d95659455582588e49ef2b419
Raymon del2 <wambuiraymond03@gmail.com>
2026-02-08 13:54:22 +0300

    fix: use passthrough image service to avoid Sharp dependency

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,11 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bloom-language.vercel.app',
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/noop',
+		},
+	},
 	integrations: [
 		starlight({
 			title: 'Bloom',
